35e7d58df6114e4f8b84b7f2e7108290a882345e
caiyingying <caiyingying234@gmail.com>
2013-12-24 20:12:45 +0800

    集成mybatis

---
+++ appServer/src/com/krakentouch/server/bootstrap/BootstrapClass.java
@@ -0,0 +1,31 @@
+package com.krakentouch.server.bootstrap;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BootstrapClass {
+
+	public static void main(String[] args) {
+		/*try {
+			NioSocketAcceptor acceptor = new NioSocketAcceptor();
+			
+			ServerHandler serverHandler = new ServerHandler();
+			
+			acceptor.setHandler(serverHandler);
+			
+			// The logger, if needed. Commented atm
+			DefaultIoFilterChainBuilder chain = acceptor.getFilterChain();
+			
+			chain.addLast("logger", new LoggingFilter());
+			
+			SocketSessionConfig scfg = acceptor.getSessionConfig();
+			
+			acceptor.bind(new InetSocketAddress(18567));
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}*/
+		ClassPathXmlApplicationContext ct =    new ClassPathXmlApplicationContext("applicationContext.xml");     
+		System.out.println("Server started...");
+	}
+
+}
---
+++ appServer/src/com/krakentouch/server/filter/ServerCodec.java
@@ -0,0 +1,16 @@
+package com.krakentouch.server.filter;
+
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.ProtocolDecoder;
+import org.apache.mina.filter.codec.ProtocolEncoder;
+
+public class ServerCodec extends ProtocolCodecFilter {
+	
+	private static ProtocolEncoder encoder = new ServerEncoder();
+	private static ProtocolDecoder decoder = new ServerDecoder();
+	
+	public ServerCodec() {
+		super(encoder, decoder);
+	}
+	
+}
