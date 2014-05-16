6d3b7ac82cdf5c4f9bd2ca0997dc7a48fcc933d5
caiyingying <caiyingying234@gmail.com>
2014-05-16 22:39:08 +0800

    结分的通知

--- appServer/src/com/krakentouch/server/action/impl/ConcludeGameActionImpl.java
+++ appServer/src/com/krakentouch/server/action/impl/ConcludeGameActionImpl.java
@@ -1,8 +1,10 @@
 package com.krakentouch.server.action.impl;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.mina.core.session.IoSession;
 
@@ -11,9 +13,15 @@ import com.krakentouch.server.bean.CloseStageBean;
 import com.krakentouch.server.bean.CloseStageBeanValue;
 import com.krakentouch.server.domain.SeatMap;
 import com.krakentouch.server.service.GameService;
-import com.krakentouch.server.service.LoginService;
 import com.krakentouch.server.tools.JaxbUtil;
-
+/***
+ * 首先由桌主发出
+ * <TCP action="ConcludeGame" address="AP" category="Game" value="StageSN=1;PlayerID=ABCDEFGHIJ"></TCP>
+ * 服务器端对本游戏桌的所有用户（通过查询SeatMap表获知）发出下行通知：
+ * <TCP action="ConcludeGame" address="NT" category="Game" value="StageSN=1"></TCP>
+ * @author CYY
+ *
+ */
 public class ConcludeGameActionImpl implements ConcludeGameAction {
 	
 	private GameService gameService;
@@ -24,15 +32,16 @@ public class ConcludeGameActionImpl implements ConcludeGameAction {
 		//得到座位的上的用户
 		String command = commandMap.get("Command");
 		String stageSN = commandMap.get("StageSN");
-		String playerID = commandMap.get("PlayerID");
+//		String playerID = commandMap.get("PlayerID");
 		
+		//需要通知的用户
+		Set<String> playerIdSet = new HashSet<String>();
 		//得到同桌的玩家
 		List<SeatMap> seatList = gameService.querySeatMapByStageSN(stageSN);
-		Collection<IoSession> sessions = session.getService().getManagedSessions().values();
-		for(IoSession s : sessions){
-			//session.getAttribute("playerId");
-			//TODO 把没个用户的用户id写到session中
+		for(SeatMap seat:seatList){
+			playerIdSet.add(seat.getPlayerID());
 		}
+		
 		
 		CloseStageBean closeStageBean = new CloseStageBean();
 		closeStageBean.setCommand(command);
@@ -44,7 +53,15 @@ public class ConcludeGameActionImpl implements ConcludeGameAction {
 		closeStageBean.setCloseStageBeanValue(closeStageBeanValue );
 		
 		retStr = JaxbUtil.convertToXml(closeStageBean, "utf-8");
-		session.write(retStr);
+		//session.write(retStr);
+		
+		Collection<IoSession> sessions = session.getService().getManagedSessions().values();
+		for(IoSession s : sessions){
+			String tempPlayerId = (String) s.getAttribute("playerId");
+			if(playerIdSet.contains(tempPlayerId)){
+				s.write(retStr);
+			}
+		}
 		return retStr;
 	}
 
--- appServer/src/com/krakentouch/server/bean/CloseStageBean.java
+++ appServer/src/com/krakentouch/server/bean/CloseStageBean.java
@@ -10,7 +10,7 @@ import javax.xml.bind.annotation.XmlType;
 @XmlRootElement(name="TCP")
 @XmlType(name = "CloseStageBean", propOrder = { "command", "result", "note", "closeStageBeanValue"})
 public class CloseStageBean {
-	@XmlElement(name = "Command", required = true)
+	@XmlElement(name = "action", required = true)
 	private String command; // <Command>login</Command>
 	
 	@XmlElement(name = "Result", required = true)
