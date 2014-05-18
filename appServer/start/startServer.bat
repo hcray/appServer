deab891bc0898f8822a40be35528ee976b7d812d
caiyingying <caiyingying234@gmail.com>
2014-05-18 18:11:43 +0800

    运行jvm参数的配置

--- appServer/start/startServer.bat
+++ appServer/start/startServer.bat
@@ -5,5 +5,6 @@ if exist "%CURRENT_DIR%\appServer.jar" goto okJar
 echo not find appClient.jar
 :okJar
 title appServer
-java -jar %CURRENT_DIR%\appServer.jar
+set JAVA_OPTS = -Xms256m -Xmx512m -XX:PermSize=128M -XX:MaxPermSize=256M 
+java %JAVA_OPTS% -jar %CURRENT_DIR%\appServer.jar
 echo end
